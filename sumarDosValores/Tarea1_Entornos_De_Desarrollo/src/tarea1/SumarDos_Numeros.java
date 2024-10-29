36ed5942c3ae483394335c05919c872ca1fa2168
Agustin Bernal <abersit270@g.educaand.es>
2024-10-29 10:01:32 +0100

    Modificamos doc1.txt

--- sumarDosValores/Tarea1_Entornos_De_Desarrollo/src/tarea1/SumarDos_Numeros.java
+++ sumarDosValores/Tarea1_Entornos_De_Desarrollo/src/tarea1/SumarDos_Numeros.java
@@ -5,10 +5,21 @@ public class SumarDos_Numeros {
 	public static void main(String[]args) {
 		System.out.println("Ingrese dos números para sumarlos");
 		Scanner lector=new Scanner(System.in);
+		
+		System.out.println("Introduzca 2 valores para realizar la suma");
+
 		double valor1=Double.parseDouble(lector.nextLine());
 		double valor2=Double.parseDouble(lector.nextLine());
-		
-		System.out.println("La suma de sus valores es" +valor1+valor2);
+		double resultado= valor1+valor2;
+		System.out.println("La suma de sus valores es " + resultado);
 		lector.close();
+
+		if(resultado%2==0){
+			System.out.println("Su número es PAR");
+		}
+		else{
+			System.out.println("Su número es impar");
+		}
+		
 	}
 }
